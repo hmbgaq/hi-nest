65d91ac6a37c8d2586a79f2bc3378677a70bb406
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-22 00:02:16 +0900

    NestJs

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,14 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { IsNumber, IsString } from "class-validator";
+import { CreateMovieDTO } from "./create-movie.dto";
+
+export class UpdateMovieDTO extends PartialType(CreateMovieDTO) {
+    // @IsString()
+    // readonly title?: string;
+
+    // @IsNumber()
+    // readonly year?: number;
+    
+    // @IsString({ each: true })
+    // readonly genres?: string[];
+}
